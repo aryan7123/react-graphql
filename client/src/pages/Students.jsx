a5c5f07975b0583bb80b2b64ff9fbf766bca468c
Raza Ali Rahman <aryanrahman90277@gmail.com>
2024-01-07 23:39:51 +0530

    table created in students page

--- client/src/pages/Students.jsx
+++ client/src/pages/Students.jsx
@@ -1,11 +1,169 @@
-import Header from "../components/Header"
+import Header from "../components/Header";
+import { Card, Typography } from "@material-tailwind/react";
 
 const Students = () => {
   return (
     <>
-        <Header />
-    </>
-  )
-}
+      <Header />
 
-export default Students
+      <Card className="max-w-6xl mx-auto px-10 md:px-0 mt-10 rounded-md md:overflow-hidden overflow-x-scroll">
+        <table className="w-[inherit] md:w-full table-auto text-left">
+          <thead>
+            <tr>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  S No.
+                </Typography>
+              </th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  Name
+                </Typography>
+              </th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  Age
+                </Typography>
+              </th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  Mobile Number
+                </Typography>
+              </th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  Subject
+                </Typography>
+              </th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  Project Name
+                </Typography>
+              </th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  Project Status
+                </Typography>
+              </th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
+                  Action
+                </Typography>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="p-4 border-b border-blue-gray-50">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  1
+                </Typography>
+              </td>
+              <td className="p-4 border-b border-blue-gray-50">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  Aryan
+                </Typography>
+              </td>
+              <td className="p-4 border-b border-blue-gray-50">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  22
+                </Typography>
+              </td>
+              <td className="p-4 border-b border-blue-gray-50">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  6201192109
+                </Typography>
+              </td>
+              <td className="p-4 border-b border-blue-gray-50">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  CSE
+                </Typography>
+              </td>
+              <td className="p-4 border-b border-blue-gray-50">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  GraphQL
+                </Typography>
+              </td>
+              <td className="p-4 border-b border-blue-gray-50">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  Pending
+                </Typography>
+              </td>
+              <td className="p-4 border-b border-blue-gray-50">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal"
+                >
+                  Edit
+                </Typography>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Card>
+    </>
+  );
+};
+
+export default Students;
